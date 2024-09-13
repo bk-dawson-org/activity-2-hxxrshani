3c83908dc2770b8e85d9b8b2b9ec11a0521215b4
hxxrshani <harshani.rahulakumar@dawsoncollege.qc.ca>
2024-09-13 23:14:58 +0000

    activity 2

---
+++ activity2.py
@@ -0,0 +1,7 @@
+x=4*(5/2)**2
+print(x)
+num=9**1/2
+print(num)
+y=97+2/12-18*3%2
+print(y)
+print(round(y,4))
---
+++ tax.py
@@ -0,0 +1,5 @@
+price=float(input("Enter the price of the food:"))
+priceTaxincluded=price*0.18+price
+print("This is the price tax included:",priceTaxincluded)
+x="Harshani"*3
+print(x)
